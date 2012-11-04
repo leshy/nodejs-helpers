66fd6c0cda5c261e58f48276906ee70c6ffe6c49
lesh <lesh@sysphere.org>
2012-11-05 00:44:57 +0100

    todict, hashfilter

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 var _ = require('underscore')
 
 // calls function if it exists
-exports.cb = function() { 
+exports.cbc = function() { 
     var args = exports.toArray(arguments)
     if (!args.length) { return }
     var callback = args.shift()
     if (callback) { callback.apply(this,args) }
 }
 
-exports.cbc = function(callback) { 
-    if (callback) { return callback } else { return function () {} }
-}
+exports.cb = function(callback) { if (callback) { return callback } else { return function () {} } }
 
 // converts retarded magical arguments object to an Array object
 var toArray = exports.toArray = function(arg) { return Array.prototype.slice.call(arg); }
@@ -171,6 +169,7 @@ exports.rtrim = function (str, chars) {
 
 exports.capitalize = function (str) { return str.charAt(0).toUpperCase() + str.slice(1); }
 
+// try to iterate through an object, call a callback with the object itself if you fail
 exports.maybeiterate = function (something,callback) {
     if (!something) { callback(); return }
 
@@ -187,6 +186,7 @@ exports.maybeiterate = function (something,callback) {
     callback(something)
 }
 
+// universal map
 exports.unimap = function (something,callback) {
     if (something.constructor == Array) { return _.map(something,callback) }
     if (something.constructor == Object) { return exports.hashmap(something,callback) }
@@ -199,6 +199,13 @@ exports.hashmap = function (hash,callback) {
     return ret
 }
 
+exports.hashfilter = function (hash,callback) {
+    var ret = {}
+    for (property in hash) { var r = callback(hash[property], property); if (r) { ret[property] = r} }
+    return ret
+}
+
+// I want find to return value and a key too
 exports.find = function (collection,callback) {
     for (i in collection) {
         var ret = callback(collection[i], i)
@@ -212,10 +219,15 @@ exports.Day = exports.Hour * 24
 exports.Month = exports.Day * 30
 exports.Now = function () { return new Date().getTime() }
 
-// I want my push to return the resulting array..
-exports.push = function () {
-    var args = exports.toArray(arguments)
-    var array = args.shift()
-    array.push.apply(array,args)
-    return array
+exports.isEmpty = function (ob){
+    for(var i in ob){ return false;}
+    return true;
+}
+
+// convert an array to dict of a form { entry: true, entry2: true }
+// used in order to utilize quick searching through the dict data structure vs the array one
+exports.todict = function (array) {
+    var ret = {}
+    _.map(array, function (x) { ret[x] = true })
+    return ret
 }
